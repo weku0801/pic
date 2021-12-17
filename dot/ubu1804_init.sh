16bef694e067e9d7ac4593b88e763a12ce4e3e69
uk <weku0801@gmail.com>
2021-12-17 12:40:13 +0800

    add ubu1802_init.sh

---
+++ dot/ubu1804_init.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# apt
+sudo cp -a /etc/apt/sources.list /etc/apt/sources.list.bak
+
+sudo bash -c 'cat << UK > /etc/apt/sources.list
+# See http://help.ubuntu.com/community/UpgradeNotes for how to upgrade to newer versions of the distribution.
+deb http://repo.huaweicloud.com/ubuntu bionic main restricted
+deb http://repo.huaweicloud.com/ubuntu bionic-updates main restricted
+
+deb http://repo.huaweicloud.com/ubuntu bionic universe
+deb http://repo.huaweicloud.com/ubuntu bionic-updates universe
+
+deb http://repo.huaweicloud.com/ubuntu bionic multiverse
+deb http://repo.huaweicloud.com/ubuntu bionic-updates multiverse
+
+deb http://repo.huaweicloud.com/ubuntu bionic-backports main restricted universe multiverse
+deb http://repo.huaweicloud.com/ubuntu bionic-security main restricted
+deb http://repo.huaweicloud.com/ubuntu bionic-security universe
+deb http://repo.huaweicloud.com/ubuntu bionic-security multiverse
+UK'
+sudo apt-get update
+
+sudo apt install git zsh wget unzip tree tmux vim sysstat -y
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+sed -i 's/(git/(git zsh-syntax-highlighting zsh-autosuggestions kubectl sudo docker helm/g' ~/.zshrc
+echo 'DISABLE_AUTO_UPDATE="true"' >> ~/.zshrc
+echo 'HIST_STAMPS="yyyy-mm-dd"' >> ~/.zshrc
+echo 'alias vi="vim"' >> ~/.zshrc
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="ys"/g' ~/.zshrc
+
+sudo chsh -s /bin/zsh $USER
+zsh
